6d121d2b03bfdc2bf2a46358fc6a395f77685ab6
ErhanBalkan <erhanBalkan@outlook.com.tr>
2022-11-22 21:08:01 +0300

    Reflection konusu eklendi.

---
+++ Attributes/Program.cs
@@ -0,0 +1,61 @@
+﻿CustomerDal customerDal = new CustomerDal();
+Customer customer = new Customer{Id=1,LastName="Balkan",Age=23};
+customerDal.Add(customer);
+
+
+[ToTable("Customers")]
+[ToTable("TblCustomers")]
+// AllowMultiple = true demiştik burada kullandık bunun anlamı örnek olarak kullanım amacımız
+// Bu class bir tablo anlamını vermekti tablo adını da parametre olarak veriyoruz
+// hangi tabloyu bulursa o olmuş olacak. 
+class Customer
+{
+    public int Id { get; set; }
+    [RequiredProperty]
+    public string FirstName { get; set; }    
+    public string LastName { get; set; }
+    public int Age { get; set; }    
+}
+
+// [AttributeUsage(AttributeTargets.All)] bu attribute ister class'a ister metoda ister property'e her şeye kullanılabilir anlamını verir.
+// [AttributeUsage(AttributeTargets.Class)] 
+[AttributeUsage(AttributeTargets.Property)] 
+// [AttributeUsage(AttributeTargets.Method)] 
+// Her şeyde değil de belirtilenlerde kullanılabilir olması için pipe kullanılır;
+// [AttributeUsage(AttributeTargets.Class | AttributeTargets.Constructor)]
+
+class RequiredPropertyAttribute : Attribute
+{
+    /*
+    Attribute oluştururken isminin sonuna Attribute eklenir ve Attribute sınıfından miras alır.
+    */
+}
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+// Çoklu kullanımı destekler örneği yukarda kullanıldı
+class ToTableAttribute : Attribute
+{
+    // Attribute parametre alabilir.
+    private string _tableName;
+    public ToTableAttribute(string tableName)
+    {
+        _tableName = tableName;
+    }
+}
+
+class CustomerDal
+{
+    /*
+    Örneğin bir metot var ve projede çoğu yerde kullanılıyor onu değiştiremiyoruz ama yenisini yazdık
+    bundan sonra yenisi kullanılsın istiyoruz Obsolete hazır bir attribute'dir parametreye mesaj gireriz ve 
+    metot kullanıldığında bize uyarı verir.
+    */
+    [Obsolete("Bu eski bir metottur lütfen yeni AddNew metodunu kullan!")]
+    public void Add(Customer customer){
+        System.Console.WriteLine("{0},{1},{2},{3} added!", customer.Id, customer.FirstName, customer.LastName, customer.Age);
+    }    
+
+    public void AddNew(Customer customer){
+
+    }
+}
+
---
+++ Reflections/Program.cs
@@ -0,0 +1,71 @@
+﻿// DortIslem dortIslem = new DortIslem(2,3);
+// dortIslem.Topla2();
+// dortIslem.Topla(3,4);
+
+// Çalışacağımız tipi belirtiyoruz
+using System.Reflection;
+
+var type = typeof(DortIslem);
+
+// DortIslem dortIslem = new DortIslem(2,3);
+// üstteki new ile activator olayı aynı şeydir.
+// parametreli constructor için type,param1,param2 şeklinde belirtebiliriz.
+// DortIslem dortIslem = (DortIslem)Activator.CreateInstance(type);
+
+// System.Console.WriteLine(dortIslem.Topla(4,5)); 
+
+var instance = Activator.CreateInstance(type,4,5);
+// metod bilgilerine ulaştık ve invoke ile çalıştırdık null parametre olmadığı için.
+MethodInfo methodInfo = instance.GetType().GetMethod("Topla2");
+// invoke'De tekrar parametre olarak instance vermemizin nedeni hangi instance'nin Topla2 parametresini
+// çalıştıracağını bilmediği için. 
+System.Console.WriteLine(methodInfo.Invoke(instance,null));
+System.Console.WriteLine("-----------------------------");
+// Bütün metodlarına ulaşmak
+var methods = type.GetMethods();
+foreach (var info in methods)
+{
+    System.Console.WriteLine("Metod adı : {0}", info.Name);
+    // Metodların parametrelerine ulaşmak
+    foreach (var parameterInfo in info.GetParameters())
+    {
+        System.Console.WriteLine("Parametre : {0}", parameterInfo.Name);
+    }
+    // Attribute'lerine ulaşmak
+    foreach (var attribute in info.GetCustomAttributes())
+    {
+        System.Console.WriteLine("Attribute : {0}", attribute.GetType().Name);
+    }
+}
+class DortIslem
+{
+    private int _sayi1;
+    private int _sayi2;
+    public DortIslem(int sayi1, int sayi2)
+    {
+        _sayi1 = sayi1;
+        _sayi2 = sayi2;
+    }
+    public DortIslem(){}
+    public int Topla(int sayi1, int sayi2){
+        return sayi1 + sayi2;
+    }
+    public int Carp(int sayi1, int sayi2){
+        return sayi1 * sayi2;
+    }
+
+    [MethodName("Çarpma")]
+    public int Topla2(){
+        return _sayi1 + _sayi2;
+    }
+    public int Carp2(){
+        return _sayi1 * _sayi2;
+    }
+}
+
+public class MethodNameAttribute : Attribute{
+    public MethodNameAttribute(string name)
+    {
+        
+    }
+}
